a1a59a80f2b2ccb9fa1ed58f2037c4cf7bb32949
PaoloCatalano <paolo12587@gmail.com>
2021-01-11 13:56:45 +0000

    markdown

--- src/pages/gallery.js
+++ src/pages/gallery.js
@@ -1,18 +1,52 @@
-import React from "react"
-import SpecialNavbar from "../components/SpecialNavbar"
-import Footer from "../components/Footer"
-import Rooms from "../components/Rooms"
-import Section from "../components/Section"
+// import React from "react"
+// import SpecialNavbar from "../components/SpecialNavbar"
+// import Footer from "../components/Footer"
+// import Rooms from "../components/Rooms"
+// import Section from "../components/Section"
 
-const Gallery = () => {
+// const Gallery = () => {
+//   return (
+//     <>
+//       <SpecialNavbar />
+//       <Section title="gallery" />
+//       <Rooms />
+//       <Footer />
+//     </>
+//   )
+// }
+
+// export default Gallery
+
+import React from "react"
+import { graphql } from "gatsby"
+import ReactMarkdown from "react-markdown"
+
+const ComponentName = ({ data }) => {
+  console.log(data.allContentfulPrezziCards.nodes[2].noteEn)
   return (
-    <>
-      <SpecialNavbar />
-      <Section title="gallery" />
-      <Rooms />
-      <Footer />
-    </>
+    <p>hello</p>
+    // <ReactMarkdown
+    //   source={data.allContentfulPrezziCards.nodes[2].noteEn}
+    // />
   )
 }
 
-export default Gallery
+export const query = graphql`
+  {
+    allContentfulPrezziCards {
+      nodes {
+        id
+        note
+        noteEn {
+          noteEn
+          id
+        }
+        sottotitoloEn
+        prezzoEn
+        titoloEn
+      }
+    }
+  }
+`
+
+export default ComponentName
